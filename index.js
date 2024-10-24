442ccae84004179e5f1038df935e99408a1900cb
Pedro Borba <pedroborbacontato@gmail.com>
2024-10-24 10:53:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function Operacoes() {
+    alert('Operações Matemáticas!!!')
+}
+
+Operacoes()
+
+function soma (numero1, numero2){
+    return numero1 + numero2
+}
+
+let resultadoDaSoma = soma (1, 6)
+console.log(resultadoDaSoma) //7
+
+function subtracao(numero1, numero2){
+    return numero1 - numero2
+}
+
+let resultadoDaSubtração = subtracao (17, 7)
+console.log(resultadoDaSubtração) //10
+
+function multiplicacao (numero1, numero2){
+    return numero1 * numero2
+}
+
+let resultadoDaMultiplicação = multiplicacao (4, 5)
+console.log(resultadoDaMultiplicação) //20
+
+function divisao (numero1,numero2){
+    return numero1 / numero2
+}
+
+let resultadoDaDivisão = divisao(25, 5)
+console.log(resultadoDaDivisão) //5
+
+
+
+
